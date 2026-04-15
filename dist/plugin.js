75170e3ab856a4a4d140e9e5ec268802ce16e4bf
maartenverbandt <maarten.verbandt@kuleuven.be>
2026-04-15 21:26:39 +0200

    Refactor plugin.js to return a function component

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,20 +1,20 @@
 function l({ React: e }) {
-  return e.createElement(
-    "div",
-    null,
-    "Hello from robOS plugin"
-  );
+  return function() {
+    return e.createElement(
+      "div",
+      null,
+      "Hello from robOS plugin"
+    );
+  };
 }
-const n = {
+const o = {
   manifest: {
     id: "hello-world",
     name: "Hello World",
     version: "1.0.0"
   },
-  create({ React: e }) {
-    return l({ React: e });
-  }
+  create: l
 };
 export {
-  n as default
+  o as default
 };
