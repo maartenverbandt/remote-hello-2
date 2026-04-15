7cca2efdd12628346d16b7bf9ed6fa226cf9ad7c
Maarten <maarten.verbandt@atlascopco.com>
2026-04-15 19:57:10 +0200

    first commit

---
+++ dist/plugin.js
@@ -0,0 +1,15 @@
+import { jsx as o } from "react/jsx-runtime";
+function l() {
+  return /* @__PURE__ */ o("div", { children: "Hello from robOS plugin" });
+}
+const e = {
+  manifest: {
+    id: "hello-world",
+    name: "Hello World",
+    version: "1.0.0"
+  },
+  component: l
+};
+export {
+  e as default
+};
